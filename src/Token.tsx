75d0aa8f230e7681efe7d31906d2ac4ffacb083d
Egge <egge@getcurrent.io>
2024-06-20 17:46:34 +0200

    added spent state check

--- src/Token.tsx
+++ src/Token.tsx
@@ -1,18 +1,54 @@
-import { getDecodedToken } from "@cashu/cashu-ts";
-import { useMemo } from "react";
+import { CashuMint, CashuWallet, getDecodedToken } from "@cashu/cashu-ts";
+import { useEffect, useMemo, useState } from "react";
 import QRCode from "react-qr-code";
+import sorry from "./assets/sorry.webp";
 
 type TokenProps = {
   hash: string;
 };
 
 function Token({ hash }: TokenProps) {
+  const [loading, setIsLoading] = useState(true);
+  const [isSpent, setIsSpent] = useState(false);
+
   const token = hash.slice(1);
   const decodedToken = useMemo(() => {
     return getDecodedToken(token);
   }, [token]);
-  const proofs = decodedToken.token.map((t) => t.proofs).flat();
+  const proofs = useMemo(() => {
+    return decodedToken.token.map((t) => t.proofs).flat();
+  }, [decodedToken]);
   const amount = proofs.reduce((a, c) => a + c.amount, 0);
+  useEffect(() => {
+    async function checkTokenStatus() {
+      const minturl = decodedToken.token[0].mint;
+      const wallet = new CashuWallet(new CashuMint(minturl));
+      const spent = await wallet.checkProofsSpent([
+        {
+          secret: decodedToken.token[0].proofs[0].secret,
+        },
+      ]);
+      if (spent[0]) {
+        setIsSpent(true);
+      }
+      setIsLoading(false);
+    }
+    if (decodedToken) {
+      checkTokenStatus();
+    }
+  }, [decodedToken]);
+
+  if (loading) {
+    return <p className="animate-pulse">Loading...</p>;
+  }
+  if (isSpent) {
+    return (
+      <div className="flex flex-col items-center gap-2">
+        <p>Token already spent...</p>
+        <img src={sorry} className="w-64 h-64 rounded" />
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col items-center gap-4">
       <div className="text-center">
